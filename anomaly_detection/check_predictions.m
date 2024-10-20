e293cb2c07fc690377a84a12f979b07e57ad7fa4
Vlada Pulbere <pulberevlada@gmail.com>
2024-10-20 19:15:18 +0300

    Delete anomaly_detection directory

--- anomaly_detection/check_predictions.m
+++
@@ -1,7 +0,0 @@
-function [false_positives, false_negatives, true_positives] = check_predictions(predictions, truths)
-    % Calculating false positives, false negatives, and true positives based on pred and test columns
-    false_positives = sum((predictions == 1) & (truths == 0));
-    false_negatives = sum((predictions == 0) & (truths == 1));
-    true_positives = sum((predictions == 1) & (truths == 1));
-endfunction
-

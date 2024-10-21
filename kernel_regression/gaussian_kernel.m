194fa0bccf1e7a20bc23ddea9102d671678c5b12
Vlada Pulbere <pulberevlada@gmail.com>
2024-10-21 14:09:39 +0300

    Update gaussian_kernel.m

--- kernel_regression/gaussian_kernel.m
+++ kernel_regression/gaussian_kernel.m
@@ -1,4 +1,4 @@
 function retval = gaussian_kernel (x, y, sigma)
-  # TO DO: implement gaussian kernel function
+  # gaussian kernel function
   retval = exp(-norm(x-y)^2 / (2 * sigma^2));
 endfunction

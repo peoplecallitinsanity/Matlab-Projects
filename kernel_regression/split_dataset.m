cee7d2d497921e62434297285167d8a586478488
Vlada Pulbere <pulberevlada@gmail.com>
2024-10-21 14:11:19 +0300

    Update split_dataset.m

--- kernel_regression/split_dataset.m
+++ kernel_regression/split_dataset.m
@@ -1,8 +1,4 @@
 function [X_train, y_train, X_pred, y_pred] = split_dataset (X, y, percentage)
-  # TO DO: împarte setul de date în 2 seturi:
-  # un set de training si un set de test,
-  # ambele reprezentate printr-o matrice de features un vector de clase
-  # percentage este un parametru considerat intre 0 si 1
 
   n = round(percentage * size(y, 1));
   X_train = X(1:n, :);

6fe355c8dd5d69039c0f3616710f63eaf7c320e0
Vlada Pulbere <pulberevlada@gmail.com>
2024-10-21 14:09:57 +0300

    Update get_lower_inverse.m

--- kernel_regression/get_lower_inverse.m
+++ kernel_regression/get_lower_inverse.m
@@ -1,6 +1,5 @@
 function [P] = get_lower_inverse (L)
-  # TO DO: Determinati printr-o metoda matriceala neiterativa inversa
-  # unei matrici de tipul lower
+  # inversa unei matrici de tipul lower
   n = size(L, 1);
   I = eye(n);
 

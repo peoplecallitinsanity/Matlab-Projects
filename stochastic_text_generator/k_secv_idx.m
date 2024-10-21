06fcb53df9b6938d212cb7adfde2da04907cedd5
Vlada Pulbere <pulberevlada@gmail.com>
2024-10-21 14:07:06 +0300

    Update k_secv_idx.m

--- stochastic_text_generator/k_secv_idx.m
+++ stochastic_text_generator/k_secv_idx.m
@@ -1,7 +1,5 @@
 function retval = k_secv_idx (distinct_k_sec)
     % return disctionary containing the indices of each elem from distinct_k_sec
-    % HINT: containers.Map
-    % Try not to set the key-value pairs iteratively, use containers.Map(keys, values) whenever possible
     % getting size
     size = numel(distinct_k_sec);
     % saving keys
